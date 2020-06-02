9fadb46a09a20de5158f10b0468175cdd1dfe9bb
Mike Wolf <mw@mike-wolf.com>
2020-06-02 01:52:23 -0400

    Able to join and split streams

--- src/LocalStreamVideo.js
+++ src/LocalStreamVideo.js
@@ -1,26 +1,66 @@
 import React, { useState, useRef, useEffect } from "react";
-import { getLocalStream } from "./StreamUtils";
+import { getLocalStream, combineStreams, splitStream } from "./StreamUtils";
 import Timer from "./Timer";
-let theLocalStream = getLocalStream();
+
+let SimpleVideo = ({ stream, height = 200 }) => {
+  const videoRef = useRef(null);
+  useEffect(() => {
+    if (videoRef && stream) {
+      videoRef.current.srcObject = stream;
+    }
+  }, [videoRef, stream]);
+  return (
+    <React.Fragment>
+      <div>
+        <video
+          // onPlay={() => setDisplayMode("block")}
+          ref={videoRef}
+          src={stream}
+          autoPlay
+          height={height}
+          //  width={width}
+          controls
+        />
+      </div>
+    </React.Fragment>
+  );
+};
 let LocalStream = ({ display = true, height = 200, width = 400 }) => {
   const videoRef = useRef(null);
   const [displayMode, setDisplayMode] = useState("none");
+  const [timerStream, setTimerStream] = useState(null);
+  const [theLocalStream, setTheLocalStream] = useState(null);
+  const [combinedStream, setCombimedStream] = useState(null);
+  const [splitStreams, setSplitStreams] = useState(null);
 
   useEffect(() => {
-    if (videoRef) {
+    setTheLocalStream(getLocalStream());
+  }, []);
+  useEffect(() => {
+    combineStreams(theLocalStream, timerStream).then(combo => {
+      setCombimedStream(combo);
+    });
+  }, [theLocalStream, timerStream]);
+  useEffect(() => {
+    if (combinedStream != null) {
+      setSplitStreams(splitStream(combinedStream));
+    }
+  }, [combinedStream]);
+  useEffect(() => {
+    if (videoRef && theLocalStream) {
       theLocalStream
         .then(stream => {
           videoRef.current.srcObject = stream;
         })
         .catch(e => console.log(e));
     }
-  }, [videoRef]);
+  }, [videoRef, theLocalStream]);
 
   return (
     <React.Fragment>
       <div style={{ display: displayMode }}>
         <div style={{ position: "absolute" }}>
-          <Timer />
+          <Timer setStream={setTimerStream} />
         </div>
         <video
           onPlay={() => setDisplayMode("block")}
@@ -31,6 +71,13 @@ let LocalStream = ({ display = true, height = 200, width = 400 }) => {
           controls
         />
       </div>
+      <SimpleVideo stream={combinedStream} autoPlay />
+      {splitStreams ? (
+        <div>
+          <SimpleVideo stream={splitStreams[0]} height={40} autoPlay />
+          <SimpleVideo stream={splitStreams[1]} height={40} />
+        </div>
+      ) : null}
     </React.Fragment>
   );
 };
--- src/StreamUtils.js
+++ src/StreamUtils.js
@@ -1,14 +1,47 @@
-var constraints = {
-  audio: true,
-  video: true
-};
+import React, { useRef, useEffect, useState } from "react";
+export function getLocalStream() {
+  var constraints = {
+    audio: true,
+    video: true
+  };
 
-let getLocalStream = () => {
   return navigator.mediaDevices
     .getUserMedia(constraints)
     .catch(e => console.log(e));
-};
+}
+export function combineStreams(stream1, stream2) {
+  return Promise.all([stream1, stream2]).then(([stream1, stream2]) => {
+    if (stream1 && stream2) {
+      const tracks = stream2.getTracks();
+      tracks.forEach(track => stream1.addTrack(track));
+      return stream1;
+    }
+  });
+  // .then((result)=>(console.log(result)));
+}
 
-getLocalStream().then(stream => console.log("stream:", stream));
+export function splitStream(stream) {
+  console.log(stream);
+  const audioTracks = stream.getAudioTracks();
+  const videoTracks = stream.getVideoTracks();
+  // return [0, 1].map(index => new MediaStream([audioTracks[0], videoTracks[1]]));
+  return [0, 1].map(index => {
+    const tracks = [];
+    const pushTrack = track => {
+      if (track) tracks.push(track);
+    };
+    pushTrack(audioTracks[index]);
+    pushTrack(videoTracks[index]);
+    return new MediaStream(tracks);
+  });
+}
 
-export { getLocalStream };
+export function TrackVideo({ track, height = 50 }) {
+  const videoRef = useRef(null);
+  useEffect(() => {
+    if (videoRef && track) {
+      videoRef.current.srcObject = track;
+    }
+  }, [videoRef, track]);
+  return <video ref={videoRef} autoPlay controls />;
+}
